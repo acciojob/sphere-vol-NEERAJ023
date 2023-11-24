2cae6398906ddb68f6b19f6e41d8823450d25add
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-24 15:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     //Write your code here
   let r=document.getElementById('MyForm').radius.value;
 	
-	document.getElementById('MyForm').volume.value=(4/3)*Math.PI*r*r*r;
+	document.getElementById('MyForm').volume.value=((4/3)*Math.PI*r*r*r).toFixed(4);
 	 event.preventDefault();
 } 
 
